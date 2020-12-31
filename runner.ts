b132b1e73285df30344444d711c4b6bd19f48b22
Joe Gibbs Politz <joe.politz@gmail.com>
2020-12-31 12:48:59 -0800

    simplest web-based REPL, needs styling

--- runner.ts
+++ runner.ts
@@ -6,11 +6,19 @@
 import wabt from 'wabt';
 import * as compiler from './compiler';
 
-const oldProcessOn = process.on;
-process.on = ((...args : any) : void => {
-  if(args[0] === "uncaughtException") { return; }
-  else { return oldProcessOn.apply(process, args); }
-}) as any;
+// NOTE(joe): This is a hack to get the CLI Repl to run. WABT registers a global
+// uncaught exn handler, and this is not allowed when running the REPL
+// (https://nodejs.org/api/repl.html#repl_global_uncaught_exceptions). No reason
+// is given for this in the docs page, and I haven't spent time on the domain
+// module to figure out what's going on here. It doesn't seem critical for WABT
+// to have this support, so we patch it away.
+if(typeof process !== "undefined") {
+  const oldProcessOn = process.on;
+  process.on = ((...args : any) : void => {
+    if(args[0] === "uncaughtException") { return; }
+    else { return oldProcessOn.apply(process, args); }
+  }) as any;
+}
 
 export async function run(source : string, config: any) : Promise<compiler.GlobalEnv> {
   const wabtInterface = await wabt();
--- webstart.ts
+++ webstart.ts
@@ -1,23 +1,54 @@
-import {run} from './runner';
+import {BasicREPL} from './repl';
 import {emptyEnv} from './compiler';
+import { output } from './webpack.config';
+
 
 function webStart() {
   document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("run").addEventListener("click", function(e) {
-      var importObject = {
-        imports: {
-          imported_func: (arg : any) => {
-            console.log("Logging from WASM: ", arg);
-            const elt = document.createElement("pre");
-            document.getElementById("output").appendChild(elt);
-            elt.innerText = arg;
-          }
-        }
-      };
-      const env = emptyEnv;
 
+    var importObject = {
+      imports: {
+        imported_func: (arg : any) => {
+          console.log("Logging from WASM: ", arg);
+          const elt = document.createElement("pre");
+          document.getElementById("output").appendChild(elt);
+          elt.innerText = arg;
+        }
+      }
+    };
+    const env = emptyEnv;
+    var repl = new BasicREPL(importObject);
+
+
+    function setupRepl() {
+      document.getElementById("output").innerHTML = "";
+      const replCodeElement = document.getElementById("next-code") as HTMLInputElement;
+      replCodeElement.addEventListener("keypress", (e) => {
+        if(e.key === "Enter") {
+          const output = document.createElement("div");
+          const prompt = document.createElement("span");
+          prompt.innerText = "»";
+          output.appendChild(prompt);
+          const elt = document.createElement("input");
+          elt.type = "text";
+          elt.disabled = true;
+          elt.className = "repl-code";
+          output.appendChild(elt);
+          document.getElementById("output").appendChild(output);
+          const source = replCodeElement.value;
+          elt.value = source;
+          replCodeElement.value = "";
+          repl.run(source).then(() => console.log ("run finished")).catch((e) => console.log("run failed", e));;
+        }
+      });
+    }
+
+
+    document.getElementById("run").addEventListener("click", function(e) {
+      repl = new BasicREPL(importObject);
       const source = document.getElementById("user-code") as HTMLTextAreaElement;
-      run(source.value, { importObject, env }).then(() => console.log ("run finished")).catch((e) => console.log("run failed", e));
+      setupRepl();
+      repl.run(source.value).then(() => console.log ("run finished")).catch((e) => console.log("run failed", e));
     });
   });
 }
